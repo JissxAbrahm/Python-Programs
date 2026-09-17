07c2b11813573907d0bba5d41224be35337f1635
JissxAbraham <90022589+JissxAbrahm@users.noreply.github.com>
2026-09-17 23:22:17 +0530

    Create LCM of three nos.py

---
+++ OTH/LCM of three nos.py
@@ -0,0 +1,9 @@
+import math
+
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+c = int(input("Enter third number: "))
+
+lcm = math.lcm(a, b, c)
+
+print("LCM =", lcm)
